7b2ba9d61f2c2bf2d7bb1550977fbf4fcc5225cf
anshyyy <anshuman9998@gmail.com>
2025-05-09 15:15:16 +0530

    Update project configuration: add dotenv dependency, include NewsModule in AppModule, and modify server startup to use environment port

---
+++ src/config/env.ts
@@ -0,0 +1,17 @@
+import { config as dotenvConfig } from 'dotenv'; 
+
+dotenvConfig({ path: '.env' });
+
+export const env = {
+  port: process.env.PORT,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
+  twitter: {
+    clientId: process.env.TWITTER_CLIENT_ID,
+    clientSecret: process.env.TWITTER_CLIENT_SECRET,
+  },
+};
